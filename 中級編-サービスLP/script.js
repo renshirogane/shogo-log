0b2758cf3260299aade78f111b018ad9161f42f1
yuushirogane <yhitm1114@gmail.com>
2024-08-26 21:31:38 +0900

    中級編-サービスLPです。

---
+++ 中級編-サービスLP/script.js
@@ -0,0 +1,8 @@
+"use strict"
+new Splide( '.splide' ).mount();
+
+$(".accordion_force_clicked").click(function () {
+  $(".accordion_action_clicked_icon").toggleClass('_emergent');
+  $(".accordion_action_clicked").slideToggle();
+});
+
