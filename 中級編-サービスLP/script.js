9bae429740614358f4ab1d92ecd021ce1d02ae56
yuushirogane <yhitm1114@gmail.com>
2024-08-27 09:03:18 +0900

    Delete 中級編-サービスLP directory

--- 中級編-サービスLP/script.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-new Splide( '.splide' ).mount();
-
-$(".accordion_force_clicked").click(function () {
-  $(".accordion_action_clicked_icon").toggleClass('_emergent');
-  $(".accordion_action_clicked").slideToggle();
-});
-
