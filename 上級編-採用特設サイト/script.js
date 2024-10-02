aec2329028eeba70d55739466e6bd57cb16983c2
yuushirogane <yhitm1114@gmail.com>
2024-10-02 11:41:03 +0900

    10/1

--- 上級編-採用特設サイト/script.js
+++ 上級編-採用特設サイト/script.js
@@ -1,10 +1,13 @@
 "use strict"
+
+"use strict"
 const splide = new Splide(".splide", {
   type: "loop", // スライダーをループさせる
   pauseOnHover: false, // カーソルが乗っても停止しない
   pauseOnFocus: false, // フォーカスを当てても停止しない
   interval: 2000, // 自動再生の間隔
   speed: 1000, // スライドの移動速度
+  perPage: 3, // デフォルトの表示枚数
   breakpoints: {
     1080: {
       perPage: 1, // 1015px未満では1枚だけ表示
@@ -14,13 +17,9 @@ const splide = new Splide(".splide", {
 }).mount();
 
 
-// Accordionとハンバーガーメニューのイベントリスナーはそのまま
-$(".accordion_force_clicked").click(function () {
-  var faqBox = $(this).closest(".faq__box");
-  faqBox.find(".accordion_action_clicked_icon").toggleClass('_emergent');
-  faqBox.find(".accordion_action_clicked").slideToggle();
-});
-
+// ハンバーガーメニューのイベントリスナーはそのまま
 $(".hamburger_force_clicked").click(function () {
   $(".hamburger_action_clicked").toggleClass('_emergent');
 });
+
+
