252d527c63322e0d17ccd2ec8fd5ced7674145f5
REN <yhitm1114@gmail.com>
2024-10-21 19:46:51 +0900

    10/21

--- 上級編-採用特設サイト/編集/script.js
+++ 上級編-採用特設サイト/編集/script.js
@@ -47,13 +47,9 @@ $(document).ready(function() {
   }
 });
 
-// Accordionとハンバーガーメニューのイベントリスナーはそのまま
+// Accordionとハンバーガーメニューのイベントリスナー
 $(".accordion_force_clicked").click(function () {
   var faqBox = $(this).closest(".faq__box");
   faqBox.find(".accordion_action_clicked_icon").toggleClass('_emergent');
   faqBox.find(".accordion_action_clicked").slideToggle();
-});
-
-$(".hamburger_force_clicked").click(function () {
-  $(".hamburger_action_clicked").toggleClass('_emergent');
 });
